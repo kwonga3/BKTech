ae35b7454fbf5c62c187a30a2d3f151107f1f6c2
kwonga3 <68578363+kwonga3@users.noreply.github.com>
2021-01-14 16:23:19 +0900

    최초 업로드

---
+++ BKTech/src/main/java/com/bktech/BkTechApplication.java
@@ -0,0 +1,13 @@
+package com.bktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkTechApplication.class, args);
+	}
+
+}
---
+++ BKTech/src/main/java/com/bktech/controller/homeController.java
@@ -0,0 +1,62 @@
+package com.bktech.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+	
+	@RequestMapping("/")
+	public String main() {
+		
+		return "index.html";
+	}
+	
+	@RequestMapping("/about")
+	public String about() {
+		
+		return "about.html";
+	}
+	
+	@RequestMapping("/portfolio")
+	public String portfolio() {
+		
+		return "portfolio.html";
+	}
+	
+	@RequestMapping("/certification")
+	public String certification() {
+		
+		return "certification.html";
+	}
+	
+	@RequestMapping("/contact")
+	public String contact() {
+		
+		return "contact.html";
+	}
+	@RequestMapping("/catalog")
+	public String catalog() {
+		
+		return "catalog.html";
+	}
+	
+	@RequestMapping("/braket")
+	public String braket() {
+		
+		return "braket.html";
+	}
+	
+	@RequestMapping("/pole")
+	public String pole() {
+		
+		return "pole.html";
+	}
+	
+	@RequestMapping("/hamche")
+	public String hamche() {
+		
+		return "hamche.html";
+	}
+
+}
