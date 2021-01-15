6eca9cce426b26ecba88fdd375e9d409f829bc72
kwonga3 <68578363+kwonga3@users.noreply.github.com>
2021-01-15 17:13:38 +0900

    수정

---
+++ BKTech/src/test/java/com/bktech/home/BkTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bktech.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
