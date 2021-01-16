c7c8962737e633b72b0dc71c262e101aa528c78a
kwonga3 <68578363+kwonga3@users.noreply.github.com>
2021-01-16 16:33:31 +0900

    메일보내기 삭제

--- BKTech/src/main/java/com/bktech/controller/MailController.java
+++ BKTech/src/main/java/com/bktech/controller/MailController.java
@@ -1,28 +1,28 @@
-package com.bktech.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import com.bktech.dto.MailDto;
-import com.bktech.service.MailService;
-
-import lombok.AllArgsConstructor;
-
-@Controller
-@AllArgsConstructor
-public class MailController {
-	
-	private final MailService mailService;
-
-    @GetMapping("/contact")
-    public String dispMail() {
-        return "contact";
-    }
-
-    @PostMapping("/contact")
-    public void execMail(MailDto mailDto) {
-        mailService.mailSend(mailDto);
-    }
-
-}
+//package com.bktech.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//import com.bktech.dto.MailDto;
+//import com.bktech.service.MailService;
+//
+//import lombok.AllArgsConstructor;
+//
+//@Controller
+//@AllArgsConstructor
+//public class MailController {
+//	
+//	private final MailService mailService;
+//
+//    @GetMapping("/contact")
+//    public String dispMail() {
+//        return "contact";
+//    }
+//
+//    @PostMapping("/contact")
+//    public void execMail(MailDto mailDto) {
+//        mailService.mailSend(mailDto);
+//    }
+//
+//}
